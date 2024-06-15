fc6a831669d9dabea0ec92014be05047ca835fc2
raphaelreshut <raphael@reshuttech.com>
2024-06-15 14:45:48 -0300

    Arrumando o  Helper

--- Psicowise/Configuration/HelperMethods.cs
+++ Psicowise/Configuration/HelperMethods.cs
@@ -5,6 +5,12 @@
         // Helper method to extract port from URL
         public static int GetPortFromUrl(string url)
         {
+            if (url.StartsWith("http://*") || url.StartsWith("https://*"))
+            {
+                var uri = new Uri(url.Replace("*", "localhost"));
+                return uri.Port;
+            }
+
             return new Uri(url).Port;
         }
     }
