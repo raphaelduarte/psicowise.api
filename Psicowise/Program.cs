3754c2bf2f129bebbc8370610d26f6a18ed5c5eb
raphaelreshut <raphael@reshuttech.com>
2024-06-15 13:53:39 -0300

    Deploy com docker

--- Psicowise/Program.cs
+++ Psicowise/Program.cs
@@ -1,10 +1,14 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Configure Kestrel to listen on the specified URL
-
+builder.WebHost.ConfigureKestrel(options =>
+{
+    options.ListenAnyIP(49152); // Porta desejada
+    // Caso queira adicionar mais portas, você pode configurar mais listeners aqui
+    // options.ListenAnyIP(5001);
+});
 
 // Add services to the container.
-
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
